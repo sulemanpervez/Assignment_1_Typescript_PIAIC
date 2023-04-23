e095e525e1330fa19d1f8fdb7fd7f63c9ef48865
Suleman pervez <119485959+Suleman1411@users.noreply.github.com>
2023-04-23 11:13:46 +0500

    Add files via upload

---
+++ Q-4.js
@@ -0,0 +1,3 @@
+"use strict";
+let quote = "Ralph Waldo Emerson once said,\"The greatest glory in living lies not in never falling, but in rising every time we fall.\"";
+console.log(quote);
---
+++ Q-4.ts
@@ -0,0 +1,2 @@
+let quote = "Ralph Waldo Emerson once said,\"The greatest glory in living lies not in never falling, but in rising every time we fall.\"";
+console.log(quote);
