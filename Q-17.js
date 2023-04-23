c8cb037c90366862e10c9fb9b330438b3fb8c65a
Suleman pervez <119485959+Suleman1411@users.noreply.github.com>
2023-04-23 11:17:49 +0500

    Add files via upload

---
+++ Q-17.js
@@ -0,0 +1,12 @@
+"use strict";
+let Fguest = ['Arif Alvi', 'Zia', 'Junaid', 'Danayal', 'Jahazaib'];
+console.log(`Hi,`, Fguest[2], `I'm sorry Junaid Table is short we wil reach you next time `);
+Fguest.splice(2, 1);
+console.log(`Hi,`, Fguest[3], `I'm sorry Jahazaib Table is short we wil reach you next time `);
+Fguest.pop();
+console.log(`Hi,`, Fguest[2], `I'm sorry Danayal Table is short we wil reach you next time `);
+Fguest.pop();
+console.log(`Taday we invited `, Fguest, `Because table is available for two people`);
+Fguest.pop();
+Fguest.pop();
+console.log(`Empty List`, Fguest);
